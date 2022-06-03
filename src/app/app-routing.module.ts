441f37cf51b4f3af2b9af525828d910a8f95cd62
Muhammad Fahad <muhammad-fahad@outlook.com>
2022-06-03 07:59:13 +0500

    Routes created for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FacebookComponent } from './facebook/facebook.component';
+import { InstagramComponent } from './instagram/instagram.component';
+import { RedditComponent } from './reddit/reddit.component';
+import { TiktokComponent } from './tiktok/tiktok.component';
+import { VimeoComponent } from './vimeo/vimeo.component';
+import { YoutubeComponent } from './youtube/youtube.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'facebook', component: FacebookComponent },
+  { path: 'instagram', component: InstagramComponent },
+  { path: 'reddit', component: RedditComponent },
+  { path: 'tiktok', component: TiktokComponent },
+  { path: 'vimeo', component: VimeoComponent },
+  { path: 'youtube', component: YoutubeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
