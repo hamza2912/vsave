8ffda0b54c31a2b108989ba5e08c280a340fe0ad
Muhammad Fahad <muhammad-fahad@outlook.com>
2022-06-11 19:53:16 +0500

    Reddit InProgress

--- src/app/reddit/reddit.component.ts
+++ src/app/reddit/reddit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RedditService } from '../services/reddit.service';
+declare var $: any;
 
 @Component({
   selector: 'app-reddit',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reddit.component.css']
 })
 export class RedditComponent implements OnInit {
-
-  constructor() { }
+  data: any;
+  constructor(private reddit: RedditService) { }
 
   ngOnInit(): void {
   }
+  test2() {
+    var link = $(".link").val();
+    if (link !== '') {
+      this.reddit.Reddit(link + '.json').subscribe((response: any) => {
+        this.data = response[0].data.children[0].data.secure_media.reddit_video.fallback_url;
+        alert(this.data)
+      });
+    } else {
+      alert('Please insert the link to download')
+    }
+  }
+
+
 
 }
